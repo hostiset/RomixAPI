6ba45b47ba115a60458cb2d63140d4ea6053aced
Romix <roman.stetsenko.07@gmail.com>
2018-01-10 17:51:41 +1000

    Update script.js

--- 1.0.0/script.js
+++ 1.0.0/script.js
@@ -1,1 +1,12 @@
+"use strict"; //enable strict mode
 
+function $error(t) {
+  if (t === undefined||null||"") {
+    alert("Error: t is '" + t + "'");
+    
+    return "$error: Error: t is '" + t + "'";
+  } else {
+    alert("Error: " + t)
+    
+    return "$error: "
+}
